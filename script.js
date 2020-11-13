6890ca3903d2a634824d77f4cc1746b40a5a307a
Matheus Alves <matttalves@gmail.com>
2020-11-13 19:53:26 -0300

    separando arquivos, alterando um pouco conteudo

---
+++ script.js
@@ -0,0 +1,34 @@
+const inputResultado = document.getElementById("resultado");
+
+document.querySelectorAll('button').forEach((elemento) => {
+    elemento.addEventListener("click", function (e) {
+
+        e.preventDefault();
+
+        const v1 = Number(document.getElementById("v1").value);
+        const v2 = Number(document.getElementById("v2").value);
+
+        const resultado = resultadoOperacao(v1, elemento.value, v2);
+
+        inputResultado.value = resultado;
+    })
+})
+
+
+function resultadoOperacao(v1, operacao, v2) {
+
+    switch (operacao) {
+        case "+":
+            return v1 + v2;
+            break;
+        case "-":
+            return v1 - v2;
+            break;
+        case "*":
+            return v1 * v2;
+            break;
+        case "/":
+            return v1 / v2;
+            break;
+    }
+}
